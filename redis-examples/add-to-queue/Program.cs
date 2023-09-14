1da7247040a8c0c82644249a0f48e64b75aabee2
congnt <congnt47@fpt.com.vn>
2023-09-14 14:31:31 +0700

    add redis job queue

--- redis-examples/add-to-queue/Program.cs
+++ redis-examples/add-to-queue/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using StackExchange.Redis;
+
+// Connect to the Redis server
+var redis = ConnectionMultiplexer.Connect("localhost"); // Replace with your Redis server connection string
+
+// Create a Redis database instance
+var db = redis.GetDatabase();
+
+int stopCode = 0; 
+do
+{
+    Console.Write("Input job code");
+    int.TryParse(Console.ReadLine(), out stopCode);
+
+    await AddJobToQueue(db, "job code: " + stopCode);
+
+}while (stopCode != 0);
+
+redis.Close();
+
+Console.WriteLine("Press Enter to exit.");
+Console.ReadLine();
+
+static async Task AddJobToQueue(IDatabase db, string jobData)
+{
+    // Generate a unique job ID (e.g., using GUID)
+    string jobId = Guid.NewGuid().ToString();
+
+    // Add the job data to a Redis list (the job queue)
+    await db.ListLeftPushAsync("job-queue", jobData);
+
+    // Store the job ID for later tracking or removal if needed
+    await db.HashSetAsync("job-details", jobId, jobData);
+}
---
+++ redis-examples/dequeue/Program.cs
@@ -0,0 +1,37 @@
+﻿using StackExchange.Redis;
+
+// Connect to the Redis server
+var redis = ConnectionMultiplexer.Connect("localhost"); // Replace with your Redis server connection string
+
+// Create a Redis database instance
+var db = redis.GetDatabase();
+
+await ProcessJobsFromQueue(db);
+
+redis.Close();
+
+Console.WriteLine("Press Enter to exit.");
+Console.ReadLine();
+
+static async Task ProcessJobsFromQueue(IDatabase db)
+{
+    while (true)
+    {
+        // Pop a job from the Redis list (the job queue)
+        var jobData = await db.ListRightPopAsync("job-queue");
+
+        if (!jobData.IsNullOrEmpty)
+        {
+            Console.WriteLine($"Processing job: {jobData}");
+
+            // You can perform your job processing logic here
+            // For this example, we'll just simulate a delay
+            await Task.Delay(TimeSpan.FromSeconds(2));
+        }
+        else
+        {
+            Console.WriteLine("No more jobs in the queue.");
+            break;
+        }
+    }
+}
