f03bc215f2b25f16187fbfe7119dfd70078b6d15
congnt <congnt47@fpt.com.vn>
2023-09-14 08:58:52 +0700

    add redis cache examples console

---
+++ redis-examples/caching/Program.cs
@@ -0,0 +1,37 @@
+﻿
+using StackExchange.Redis;
+
+// Connect to the Redis server
+var redis = ConnectionMultiplexer.Connect("localhost"); // Replace with your Redis server connection string
+
+// Create a Redis database instance
+var db = redis.GetDatabase();
+
+// Key for caching
+string cacheKey = "myCacheKey";
+
+// Check if the data is already cached
+var cachedData = await db.StringGetAsync(cacheKey);
+
+if (!cachedData.IsNullOrEmpty)
+{
+    Console.WriteLine("Cache hit!");
+    Console.WriteLine($"Cached Data: {cachedData}");
+}
+else
+{
+    Console.WriteLine("Cache miss!");
+
+    // If data is not cached, fetch it and store it in the cache
+    string newData = "This is the data to cache.";
+    Console.WriteLine($"Fetching and caching data: {newData}");
+
+    // Store the data in the cache with a time-to-live (TTL)
+    await db.StringSetAsync(cacheKey, newData, TimeSpan.FromMinutes(5)); // Cache for 5 minutes
+}
+
+// Close the Redis connection
+redis.Close();
+
+Console.WriteLine("Press Enter to exit.");
+Console.ReadLine();
