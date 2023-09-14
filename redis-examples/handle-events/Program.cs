9d6a5c10c2298970d762c49bbb88313188d0d18b
congnt <congnt47@fpt.com.vn>
2023-09-14 14:00:02 +0700

    add redis realtime analytics

---
+++ redis-examples/handle-events/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using StackExchange.Redis;
+using System.Text.Json;
+
+// Connect to Redis
+var redis = ConnectionMultiplexer.Connect("localhost"); // Replace with your Redis server connection string
+
+// Create a Redis database instance
+var db = redis.GetDatabase();
+
+// Retrieve and process the stored click events
+while (true)
+{
+    // Pop and process click events from the Redis list
+    var clickEventData = await db.ListRightPopAsync("click-events1");
+    if (clickEventData != RedisValue.Null)
+    {
+        var clickEvent = JsonSerializer.Deserialize<CustomEvent>(clickEventData);
+
+        // Perform real-time analytics on the click event data
+        Console.WriteLine($"Catched {clickEvent.Title} code {clickEvent.EventCode}");
+    }
+
+    // Add a delay to simulate real-time processing
+    //await Task.Delay(1000);
+}
+
+class CustomEvent
+{
+    public string Title { get; set; }
+    public int EventCode { get; set; }
+}
---
+++ redis-examples/making-events/Program.cs
@@ -0,0 +1,27 @@
+﻿using StackExchange.Redis;
+using System.Text.Json;
+
+// Connect to Redis
+var redis = ConnectionMultiplexer.Connect("localhost"); // Replace with your Redis server connection string
+
+// Create a Redis database instance
+var db = redis.GetDatabase();
+
+// looping make events
+int stopCode = 0;
+do
+{
+    Console.Write("push your event number(0 for exit): ");
+    int.TryParse(Console.ReadLine(), out stopCode );
+
+    if(stopCode != 0)
+    {
+        await db.ListLeftPushAsync("click-events1", JsonSerializer.Serialize(new
+        {
+            Title = "hey, i make new event",
+            EventCode = stopCode,
+        }));
+        Console.WriteLine($"you make event {stopCode} successfully!");
+    }
+
+}while (stopCode != 0);
