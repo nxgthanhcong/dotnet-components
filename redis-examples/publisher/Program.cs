a0d2b6bf7a09f6a28565fdde5df0af08afdaa555
congnt <congnt47@fpt.com.vn>
2023-09-14 14:01:40 +0700

    add pubsub-messaging

---
+++ redis-examples/publisher/Program.cs
@@ -0,0 +1,26 @@
+﻿using StackExchange.Redis;
+
+// Connect to the Redis server
+var redis = ConnectionMultiplexer.Connect("localhost"); // Replace with your Redis server connection string
+
+// Create a Redis subscriber instance
+var subscriber = redis.GetSubscriber();
+
+string channelName = "my-channel";
+int stopCode = 0;
+
+do
+{
+    Console.Write("push your event number(0 for exit): ");
+    int.TryParse(Console.ReadLine(), out stopCode);
+
+    await subscriber.PublishAsync(channelName, $"i published {stopCode} here");
+    Console.WriteLine($"Published: {stopCode}");
+
+} while (stopCode != 0);
+
+// Close the Redis connection
+redis.Close();
+
+Console.WriteLine("Press Enter to exit.");
+Console.ReadLine();
---
+++ redis-examples/subcriber/Program.cs
@@ -0,0 +1,19 @@
+﻿using StackExchange.Redis;
+
+// Connect to the Redis server
+var redis = ConnectionMultiplexer.Connect("localhost"); // Replace with your Redis server connection string
+
+// Create a Redis subscriber instance
+var subscriber = redis.GetSubscriber();
+
+string channelName = "my-channel";
+await subscriber.SubscribeAsync(channelName, (channel, message) =>
+{
+    Console.WriteLine($"Received message on channel {channel}: {message}");
+});
+
+//// Close the Redis connection
+//redis.Close();
+
+//Console.WriteLine("Press Enter to exit.");
+Console.ReadLine();
